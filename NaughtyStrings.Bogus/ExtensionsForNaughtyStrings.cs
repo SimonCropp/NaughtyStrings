8d68657706adb7cda4ea8b48e1152d04be4b23b4
Simon Cropp <simon.cropp@gmail.com>
2018-06-16 23:20:19 +1000

    .

--- NaughtyStrings.Bogus/ExtensionsForNaughtyStrings.cs
+++ NaughtyStrings.Bogus/ExtensionsForNaughtyStrings.cs
@@ -1,13 +1,13 @@
-﻿//using Bogus;
-//using Bogus.Premium;
+﻿using Bogus;
+using Bogus.Premium;
 
-//namespace NaughtyStrings.Bogus
-//{
-//    public static class ExtensionsForNaughtyStrings
-//    {
-//        public static Naughty Naughty(this Faker faker)
-//        {
-//            return ContextHelper.GetOrSet(faker, () => new Naughty());
-//        }
-//    }
-//}
+namespace NaughtyStrings.Bogus
+{
+    public static class ExtensionsForNaughtyStrings
+    {
+        public static Naughty Naughty(this Faker faker)
+        {
+            return ContextHelper.GetOrSet(faker, () => new Naughty());
+        }
+    }
+}
