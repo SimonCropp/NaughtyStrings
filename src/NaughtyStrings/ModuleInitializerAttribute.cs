726a7b373cb613c3b3b5e17b7ee0ec914adf13fd
Simon Cropp <simon.cropp@gmail.com>
2023-03-13 23:51:51 +1100

    Polyfill

--- src/NaughtyStrings/ModuleInitializerAttribute.cs
+++
@@ -1,9 +0,0 @@
-#if(!NET5_0)
-namespace System.Runtime.CompilerServices
-{
-    [AttributeUsage(AttributeTargets.Method, Inherited = false)]
-    sealed class ModuleInitializerAttribute : Attribute
-    {
-    }
-}
-#endif
