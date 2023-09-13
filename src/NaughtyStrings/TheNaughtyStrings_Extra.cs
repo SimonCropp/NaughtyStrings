078cfbceb92e384195ca84b6e6518dc59d89e479
Simon Cropp <simon.cropp@gmail.com>
2023-09-14 09:25:46 +1000

    Update TheNaughtyStrings_Extra.cs

--- src/NaughtyStrings/TheNaughtyStrings_Extra.cs
+++ src/NaughtyStrings/TheNaughtyStrings_Extra.cs
@@ -1,35 +1,34 @@
-namespace NaughtyStrings
 #if Bogus
-.Bogus
+namespace NaughtyStrings.Bogus;
+#else
+namespace NaughtyStrings;
 #endif
+public static partial class TheNaughtyStrings
 {
-    public static partial class TheNaughtyStrings
+    /// <summary>
+    /// Special characters that are commonly used by Microsoft Word.
+    /// </summary>
+    public static IReadOnlyList<string> SpecialWordCharacters = new List<string>
     {
-        /// <summary>
-        /// Special characters that are commonly used by Microsoft Word.
-        /// </summary>
-        public static IReadOnlyList<string> SpecialWordCharacters = new List<string>
-        {
-            "En dash –",
-            "Em dash —",
-            "Horizontal bar ―",
-            "Figure dash ‒",
-            "Swung dash ⁓",
-            "Single opening quote ‘",
-            "Single closing quote ’",
-            "Double opening quote “",
-            "Double closing quote ”",
-            "Copyright ©",
-            "Registered ®",
-            "Trademark ™",
-            "Section §",
-            "Paragraph ¶",
-            "Ellipsis …",
-            "Optional hyphen ¬",
-            "Non-breaking hyphen " + (char) 2011,
-            "Em space " + (char) 2003,
-            "En space " + (char) 2002,
-            "Quarter space " + (char) 2005
-        };
-    }
+        "En dash –",
+        "Em dash —",
+        "Horizontal bar ―",
+        "Figure dash ‒",
+        "Swung dash ⁓",
+        "Single opening quote ‘",
+        "Single closing quote ’",
+        "Double opening quote “",
+        "Double closing quote ”",
+        "Copyright ©",
+        "Registered ®",
+        "Trademark ™",
+        "Section §",
+        "Paragraph ¶",
+        "Ellipsis …",
+        "Optional hyphen ¬",
+        "Non-breaking hyphen " + (char) 2011,
+        "Em space " + (char) 2003,
+        "En space " + (char) 2002,
+        "Quarter space " + (char) 2005
+    };
 }
